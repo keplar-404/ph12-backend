d7164bb4f0b090f31623a992971a959c6856739b
keplar-404 <shehabhossen76.services@gmail.com>
2023-12-31 23:28:07 +0000

    config file updated

--- config/config.js
+++ config/config.js
@@ -7,12 +7,6 @@ dotenv.config();
 const config = {
   port: process.env.PORT,
   db_local: process.env.DB_LOCAL,
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
   jwt_secret: process.env.SECRET_KEY,
   refresh_secret: process.env.REFRESH_SCRET_KEY,
   stripe_scret: process.env.STRIPE_SECRET_KEY,
